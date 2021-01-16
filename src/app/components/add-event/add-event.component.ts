6b89dea4db27cd5396a6814ea17d0869a853e95f
radustavila <stavila.radu@yahoo.com>
2021-01-16 15:02:06 +0200

    added draggable marker

--- src/app/components/add-event/add-event.component.ts
+++ src/app/components/add-event/add-event.component.ts
@@ -127,6 +127,31 @@ export class AddEventComponent implements OnInit {
     });
   }
 
+  markerDragEnd($event: any) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
   private setCurrentLocation() {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
