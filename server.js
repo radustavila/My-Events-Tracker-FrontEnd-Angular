64f7ee906baa07bfe44b956671a6c127ea3d5c42
radustavila <stavila.radu@yahoo.com>
2021-04-10 16:56:41 +0300

    added for heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/my-events-tracker'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/my-events-tracker/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4201);
+
+
+
+//"start": "ng serve",
